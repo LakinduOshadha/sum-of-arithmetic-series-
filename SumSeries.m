26e3c38f445f72d0e42e1dd659ba8767c672f451
Lakindu Oshadha <59925148+LakinduOshadha@users.noreply.github.com>
2021-05-07 21:56:24 +0530

    Update SumSeries.m

--- SumSeries.m
+++ SumSeries.m
@@ -1,6 +1,6 @@
 %@author Lakindu Oshadha (lakinduoshadha98@gmail.com)
 
-function [nsum] = sumseries(n,a,d)
+function [nsum] = SumSeries(n,a,d)
 nsum = 0;
 for k = 0 : (n - 1)
     nsum = nsum + a + d*k;
